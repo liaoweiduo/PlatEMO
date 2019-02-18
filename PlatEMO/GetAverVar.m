b2ef1f3489565dcf35eab78eb671db08478624d8
liaoweiduo <liaowd@sustc.edu.cn>
2019-02-18 11:24:39 +0800

    Results_toHV_single

--- PlatEMO/GetAverVar.m
+++ PlatEMO/GetAverVar.m
@@ -1,44 +1,28 @@
 Algorithm = 'FVEMOA_DR';
-Problem = 'C1_DTLZ1';
-N = '100';
-M = '3';
 fileName=['Analysis/', Algorithm, '.mat'];
 load(fileName);
 
-hvSetStrut(1).level = -1;
-hvSetStrut(1).Rinit = 2;
-hvSetStrut(1).indexSet = Data(1).indexSet;
-hvSetStrut(1).hvSets = [];
-hvSetStrut(2).level = -0.5;
-hvSetStrut(2).Rinit = 2;
-hvSetStrut(2).indexSet = Data(1).indexSet;
-hvSetStrut(2).hvSets = [];
-hvSetStrut(3).level = 0;
-hvSetStrut(3).Rinit = 2;
-hvSetStrut(3).indexSet = Data(1).indexSet;
-hvSetStrut(3).hvSets = [];
-hvSetStrut(4).level = 0.5;
-hvSetStrut(4).Rinit = 2;
-hvSetStrut(4).indexSet = Data(1).indexSet;
-hvSetStrut(4).hvSets = [];
-hvSetStrut(5).level = 1;
-hvSetStrut(5).Rinit = 2;
-hvSetStrut(5).indexSet = Data(1).indexSet;
-hvSetStrut(5).hvSets = [];
+Problem = 'C1_DTLZ1';
+N = '100';
+M = '3';
+
+find([Data.Problem]==Problem&[Data.N]==N&[Data.M]==M)
+
+    metrics(find([metrics.Problem] == Problem && [metrics.N] == N && [metrics.M] == M))
+
+    hvSetStrut = struct('level',{-1,-0.5,0,0.5,1},'Rinit',2,'indexSet',Data(1).indexSet,'hvSets',[]);
 
 for index = 1:length(Data)
-    if (strcmp(Data(index).Algorithm, Algorithm) && strcmp(Data(index).Problem, Problem) && strcmp(Data(index).N, N) && strcmp(Data(index).M, M))
-        if (Data(index).run <= '5')
-            hvSetStrut(1).hvSets(:,size(hvSetStrut(1).hvSets,2)+1) = Data(index).hvSet;
-        elseif (Data(index).run > '5' && Data(index).run <= '10')
-            hvSetStrut(2).hvSets(:,size(hvSetStrut(2).hvSets,2)+1) = Data(index).hvSet;
-        elseif (Data(index).run > '10' && Data(index).run <= '15')
-            hvSetStrut(3).hvSets(:,size(hvSetStrut(3).hvSets,2)+1) = Data(index).hvSet;
-        elseif (Data(index).run > '15' && Data(index).run <= '20')
-            hvSetStrut(4).hvSets(:,size(hvSetStrut(4).hvSets,2)+1) = Data(index).hvSet;
-        elseif (Data(index).run > '20' && Data(index).run <= '25')
-            hvSetStrut(5).hvSets(:,size(hvSetStrut(5).hvSets,2)+1) = Data(index).hvSet;
-        end
+    if (Data(index).run <= '5')
+        hvSetStrut(1).hvSets(:,size(hvSetStrut(1).hvSets,2)+1) = Data(index).hvSet;
+    elseif (Data(index).run > '5' && Data(index).run <= '10')
+        hvSetStrut(2).hvSets(:,size(hvSetStrut(2).hvSets,2)+1) = Data(index).hvSet;
+    elseif (Data(index).run > '10' && Data(index).run <= '15')
+        hvSetStrut(3).hvSets(:,size(hvSetStrut(3).hvSets,2)+1) = Data(index).hvSet;
+    elseif (Data(index).run > '15' && Data(index).run <= '20')
+        hvSetStrut(4).hvSets(:,size(hvSetStrut(4).hvSets,2)+1) = Data(index).hvSet;
+    elseif (Data(index).run > '20' && Data(index).run <= '25')
+        hvSetStrut(5).hvSets(:,size(hvSetStrut(5).hvSets,2)+1) = Data(index).hvSet;
     end
 end
 
---
+++ PlatEMO/Results_toHV_single.m
@@ -0,0 +1,30 @@
+Algorithm = 'HypE';
+PathRoot=['Data/', Algorithm, '/'];
+filename = 'HypE_WFG9_N100_M8_D17_4.mat';
+
+filename_temp = filename(length(Algorithm)+2:length(filename));
+Problem = filename_temp(1:strfind(filename_temp,'_N')-1);
+filename_temp = filename_temp(length(Problem)+3:length(filename_temp));
+N = filename_temp(1:strfind(filename_temp,'_M')-1);
+filename_temp = filename_temp(length(N)+3:length(filename_temp));
+M = filename_temp(1:strfind(filename_temp,'_D')-1);
+filename_temp = filename_temp(length(M)+3:length(filename_temp));
+run = filename_temp(strfind(filename_temp,'_')+1:strfind(filename_temp,'.mat')-1);
+eval(['algorithmHold=', '@', Algorithm, ';']);
+eval(['problemHold=', '@', Problem, ';']);
+
+varargin = {'-algorithm', algorithmHold, '-problem', problemHold, '-N', str2num(N), '-M', str2num(M), '-run', str2num(run)};
+Global = GLOBAL(varargin{:});
+PF = Global.problem.PF(10000);
+
+clc;disp(['HV calculation: ', filename]);
+
+load(strcat(PathRoot, filename));
+indexSet = cell2mat(result(:,1));
+generationSet = result(:,2);
+parfor index = 1:length(generationSet)
+    populationSet = generationSet{index};
+    objectiveSet = populationSet.objs;
+    result(index,3) = {HV(objectiveSet, PF)};
+end
+save([PathRoot,filename],'result','metric');

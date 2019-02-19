302d5a23131f9ba9987a34cb1b06e71894a6faa3
liaoweiduo <liaowd@sustc.edu.cn>
2019-02-19 11:35:14 +0800

    drawPic.m

---
+++ PlatEMO/drawPic.m
@@ -0,0 +1,142 @@
+%% load data
+Algorithm = 'FVEMOA_DR';
+Problem = 'IDTLZ1';
+M = '3';
+fileName=['Analysis/', Algorithm, '.mat'];
+load(fileName);
+for i = 1:length(metrics)
+    if strcmp(metrics(i).Problem, Problem) && strcmp(metrics(i).M, M)
+        break
+    end
+end
+hvSetStrut = metrics(i).hvSetStrut;
+
+%% draw picture
+figure(1);
+subplot(2,2,1);
+% e1 = errorbar(hvSetStrut(1).indexSet, hvSetStrut(1).aver, hvSetStrut(1).var);
+plot(hvSetStrut(1).indexSet, hvSetStrut(1).aver);
+hold on
+% e2 = errorbar(hvSetStrut(2).indexSet, hvSetStrut(2).aver, hvSetStrut(2).var);
+plot(hvSetStrut(2).indexSet, hvSetStrut(2).aver);
+hold on
+% e3 = errorbar(hvSetStrut(3).indexSet, hvSetStrut(3).aver, hvSetStrut(3).var);
+plot(hvSetStrut(3).indexSet, hvSetStrut(3).aver);
+hold on
+% e4 = errorbar(hvSetStrut(4).indexSet, hvSetStrut(4).aver, hvSetStrut(4).var);
+plot(hvSetStrut(4).indexSet, hvSetStrut(4).aver);
+hold on
+% e5 = errorbar(hvSetStrut(5).indexSet, hvSetStrut(5).aver, hvSetStrut(5).var);
+plot(hvSetStrut(5).indexSet, hvSetStrut(5).aver);
+hold on
+legend('-1','-0.5','0','0.5','1');
+title(['FVEMOADR, IDTLZ1, M', M]);
+xlabel('Time');
+ylabel('HV');
+
+ %% load data
+Algorithm = 'FVEMOA_DR';
+Problem = 'IDTLZ1';
+M = '5';
+fileName=['Analysis/', Algorithm, '.mat'];
+load(fileName);
+for i = 1:length(metrics)
+    if strcmp(metrics(i).Problem, Problem) && strcmp(metrics(i).M, M)
+        break
+    end
+end
+hvSetStrut = metrics(i).hvSetStrut;
+
+%% draw picture
+subplot(2,2,2);
+% e1 = errorbar(hvSetStrut(1).indexSet, hvSetStrut(1).aver, hvSetStrut(1).var);
+plot(hvSetStrut(1).indexSet, hvSetStrut(1).aver);
+hold on
+% e2 = errorbar(hvSetStrut(2).indexSet, hvSetStrut(2).aver, hvSetStrut(2).var);
+plot(hvSetStrut(2).indexSet, hvSetStrut(2).aver);
+hold on
+% e3 = errorbar(hvSetStrut(3).indexSet, hvSetStrut(3).aver, hvSetStrut(3).var);
+plot(hvSetStrut(3).indexSet, hvSetStrut(3).aver);
+hold on
+% e4 = errorbar(hvSetStrut(4).indexSet, hvSetStrut(4).aver, hvSetStrut(4).var);
+plot(hvSetStrut(4).indexSet, hvSetStrut(4).aver);
+hold on
+% e5 = errorbar(hvSetStrut(5).indexSet, hvSetStrut(5).aver, hvSetStrut(5).var);
+plot(hvSetStrut(5).indexSet, hvSetStrut(5).aver);
+hold on
+legend('-1','-0.5','0','0.5','1');
+title(['FVEMOADR, IDTLZ1, M', M]);
+xlabel('Time');
+ylabel('HV');
+
+
+ %% load data
+Algorithm = 'FVEMOA_DR';
+Problem = 'IDTLZ1';
+M = '8';
+fileName=['Analysis/', Algorithm, '.mat'];
+load(fileName);
+for i = 1:length(metrics)
+    if strcmp(metrics(i).Problem, Problem) && strcmp(metrics(i).M, M)
+        break
+    end
+end
+hvSetStrut = metrics(i).hvSetStrut;
+
+%% draw picture
+subplot(2,2,3);
+% e1 = errorbar(hvSetStrut(1).indexSet, hvSetStrut(1).aver, hvSetStrut(1).var);
+plot(hvSetStrut(1).indexSet, hvSetStrut(1).aver);
+hold on
+% e2 = errorbar(hvSetStrut(2).indexSet, hvSetStrut(2).aver, hvSetStrut(2).var);
+plot(hvSetStrut(2).indexSet, hvSetStrut(2).aver);
+hold on
+% e3 = errorbar(hvSetStrut(3).indexSet, hvSetStrut(3).aver, hvSetStrut(3).var);
+plot(hvSetStrut(3).indexSet, hvSetStrut(3).aver);
+hold on
+% e4 = errorbar(hvSetStrut(4).indexSet, hvSetStrut(4).aver, hvSetStrut(4).var);
+plot(hvSetStrut(4).indexSet, hvSetStrut(4).aver);
+hold on
+% e5 = errorbar(hvSetStrut(5).indexSet, hvSetStrut(5).aver, hvSetStrut(5).var);
+plot(hvSetStrut(5).indexSet, hvSetStrut(5).aver);
+hold on
+legend('-1','-0.5','0','0.5','1');
+title(['FVEMOADR, IDTLZ1, M', M]);
+xlabel('Time');
+ylabel('HV');
+
+
+ %% load data
+Algorithm = 'FVEMOA_DR';
+Problem = 'IDTLZ1';
+M = '10';
+fileName=['Analysis/', Algorithm, '.mat'];
+load(fileName);
+for i = 1:length(metrics)
+    if strcmp(metrics(i).Problem, Problem) && strcmp(metrics(i).M, M)
+        break
+    end
+end
+hvSetStrut = metrics(i).hvSetStrut;
+
+%% draw picture
+subplot(2,2,4)d'r
+% e1 = errorbar(hvSetStrut(1).indexSet, hvSetStrut(1).aver, hvSetStrut(1).var);
+plot(hvSetStrut(1).indexSet, hvSetStrut(1).aver);
+hold on
+% e2 = errorbar(hvSetStrut(2).indexSet, hvSetStrut(2).aver, hvSetStrut(2).var);
+plot(hvSetStrut(2).indexSet, hvSetStrut(2).aver);
+hold on
+% e3 = errorbar(hvSetStrut(3).indexSet, hvSetStrut(3).aver, hvSetStrut(3).var);
+plot(hvSetStrut(3).indexSet, hvSetStrut(3).aver);
+hold on
+% e4 = errorbar(hvSetStrut(4).indexSet, hvSetStrut(4).aver, hvSetStrut(4).var);
+plot(hvSetStrut(4).indexSet, hvSetStrut(4).aver);
+hold on
+% e5 = errorbar(hvSetStrut(5).indexSet, hvSetStrut(5).aver, hvSetStrut(5).var);
+plot(hvSetStrut(5).indexSet, hvSetStrut(5).aver);
+hold on
+legend('-1','-0.5','0','0.5','1');
+title(['FVEMOADR, IDTLZ1, M', M]);
+xlabel('Time');
+ylabel('HV');

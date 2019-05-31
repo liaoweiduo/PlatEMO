61a2511b0eab0c6740be42a2d4a8c2277b8bbeb0
liaoweiduo <11849249@mail.sustech.edu.cn>
2019-05-31 22:48:16 +0800

    update

--- PlatEMO/getData_single.m
+++ PlatEMO/getData_single.m
@@ -13,7 +13,7 @@ problems = {@DTLZ1, @C1_DTLZ1, @MaF1, @IDTLZ1};
 parameters = {};
 
 M = [3,5,8,10];  
-algorithms = {@HypE_2};  % , @SMSEMOA_optima
+algorithms = {@SMSEMOA_optimal};
 
 for m = M
 for algorithm = algorithms

7e3946d2ad742962ff1b7e429689c81d1c9bea06
liaoweiduo <liaowd@mail.sustc.edu.cn>
2019-09-26 15:35:30 +0800

    update

--- PlatEMO/drawCuboid_test.m
+++ PlatEMO/drawCuboid_test.m
@@ -9,8 +9,8 @@ maxValues = max(PF,[],1);
 minValues = min(PF,[],1); 
 PF = (PF - minValues) ./ (maxValues - minValues); 
 
-% a = (1 - sum(PF,2)) ./ 3;
-% PF = PF + 1 * a; 
+a = (1 - sum(PF,2)) ./ 3;
+PF = PF + 1 * a; 
 
 maxValues = max(PF,[],1); 
 minValues = min(PF,[],1); 
@@ -27,10 +27,15 @@ for index = 1:length(PF)
     drawCuboid(point,ref);
 end
 
-scatter3(PF(:,1),PF(:,2),PF(:,3),'ro')
+scatter3(PF(:,1),PF(:,2),PF(:,3),500,'r.')
 
 view(3)
 axis square
+xlabel('f_1', 'Fontname','Times New Roman', 'FontSize',12)
+ylabel('f_2', 'Fontname','Times New Roman', 'FontSize',12)
+zlabel('f_3', 'Fontname','Times New Roman', 'FontSize',12)
+
+set(gca, 'linewidth',2,'fontsize',30,'fontname','Times')
 set(gca,'xdir','reverse');
 set(gca,'ydir','reverse');
 set(gca,'zdir','reverse');

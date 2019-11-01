393ba3756c1294f9b26b80218d4fb4db782fab9f
liaoweiduo <liaowd@mail.sustc.edu.cn>
2019-11-01 17:33:15 +0800

    update

--- PlatEMO/drawSolution.m
+++ PlatEMO/drawSolution.m
@@ -1,8 +1,8 @@
 %% draw pic of data  need data
 clear
 N = 100; M = 3; D = 12;
-algorithm = 'SMSEMOA_SS2';
-problem = 'DTLZ3';
+algorithm = 'SMSEMOA_SS2_c1';
+problem = 'DTLZ2';
 evaluation_index = 100;
 
 figure()
@@ -11,7 +11,7 @@ view(3)
 title([algorithm,' ',problem,...
     ' N',int2str(N),' M',int2str(M),' D',int2str(D)]);
 for run = 1:10
-    readPath = fullfile('Data','SMSEMOA_SS2_c1',[algorithm,'_',problem,...
+    readPath = fullfile('Data',algorithm,[algorithm,'_',problem,...
         '_N',int2str(N),'_M',int2str(M),'_D',int2str(D),'_',int2str(run),'.mat']);
     load(readPath);
     objSet = result{evaluation_index, 2}.objs;

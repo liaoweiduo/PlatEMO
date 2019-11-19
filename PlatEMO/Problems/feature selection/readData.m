a56c6aecb9b9ee8c1245d8ce70baedcded82f28a
liaoweiduo <11849249@mail.sustech.edu.cn>
2019-11-19 21:46:32 +0800

    update

--- PlatEMO/Problems/feature selection/readData.m
+++ PlatEMO/Problems/feature selection/readData.m
@@ -1,35 +1,26 @@
 clear
-obj.Global.D = 500;
+obj.Global.D = 617;
 
-fid = fopen('madelon_train.data','rt');
+fid = fopen('isolet1+2+3+4.data','rt');
 FormatString='';
 for i = 1:obj.Global.D
     FormatString = [FormatString, '%f '];
 end
-% FormatString = [FormatString, '%s'];
-n=2000;%读取数据的行数
-data_all=textscan(fid,FormatString,n,'delimiter',' '); %以逗号为数据的分隔符
+FormatString = [FormatString, '%f'];
+n=6238;%读取数据的行数
+data_all=textscan(fid,FormatString,n,'delimiter',','); %以逗号为数据的分隔符
 fclose(fid);
-fid = fopen('madelon_train.labels','rt');
-FormatString='%f';
-class=textscan(fid,FormatString,n,'delimiter',' '); %以逗号为数据的分隔符
-fclose(fid);
-data = cell2mat([class,data_all]);
-
-fid = fopen('madelon_valid.data','rt');
+data = cell2mat([data_all(end),data_all(1:end-1)]);
+fid = fopen('isolet5.data','rt');
 FormatString='';
 for i = 1:obj.Global.D
     FormatString = [FormatString, '%f '];
 end
-% FormatString = [FormatString, '%s'];
-n=600;%读取数据的行数
-data_all=textscan(fid,FormatString,n,'delimiter',' '); %以逗号为数据的分隔符
+FormatString = [FormatString, '%f'];
+n=1559;%读取数据的行数
+data_all=textscan(fid,FormatString,n,'delimiter',','); %以逗号为数据的分隔符
 fclose(fid);
-fid = fopen('madelon_valid.labels','rt');
-FormatString='%f';
-class=textscan(fid,FormatString,n,'delimiter',' '); %以逗号为数据的分隔符
-fclose(fid);
-data = [data;cell2mat([class,data_all])];
+data = [data;cell2mat([data_all(end),data_all(1:end-1)])];
 
 
 file = fullfile(fileparts(mfilename('fullpath')),'vehicle.mat');

999d91e824d5699df5d0fa2ea5c73c95f2a56cf1
liaoweiduo <11849249@mail.sustech.edu.cn>
2020-02-16 17:42:49 +0800

    update

--- PlatEMO/master/getDataFig.m
+++ PlatEMO/master/getDataFig.m
@@ -9,8 +9,8 @@ problems = {@DTLZ1, @DTLZ2, @DTLZ3, @DTLZ4,...
 
 % problems = {@WFG3};
 
-M = [3];  
-algorithms = {@SMSEMOA1,@SMSEMOA13_12,@SMSEMOA2,@SMSEMOA5}; 
+M = [10];  
+algorithms = {@SMSEMOA1,@SMSEMOA2,@SMSEMOA5,@SMSEMOA10}; 
 
 parameters = {};
 for m = M

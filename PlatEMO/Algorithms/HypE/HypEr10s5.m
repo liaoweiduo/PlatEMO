4ac0308e84258da8a6f7ad3ec11f82c93e85c786
liaoweiduo <11849249@mail.sustech.edu.cn>
2020-02-20 15:00:02 +0800

    update

---
+++ PlatEMO/Algorithms/HypE/HypEr10s5.m
@@ -0,0 +1,36 @@
+function HypEr10s5(Global)
+% <algorithm> <HypE>
+% Hypervolume estimation algorithm
+% nSample = 50000  Number of sampled points for HV estimation
+% r = 10  r of reference point
+
+%------------------------------- Reference --------------------------------
+% J. Bader and E. Zitzler, HypE: An algorithm for fast hypervolume-based
+% many-objective optimization, Evolutionary Computation, 2011, 19(1):
+% 45-76.
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2018-2019 BIMK Group. You are free to use the PlatEMO for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
+% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
+% for evolutionary multi-objective optimization [educational forum], IEEE
+% Computational Intelligence Magazine, 2017, 12(4): 73-87".
+%--------------------------------------------------------------------------
+
+    %% Parameter setting
+    nSample = 50000;
+    r = 10;
+    
+    %% Generate random population
+    Population = Global.Initialization();
+
+    %% Optimization
+    while Global.NotTermination(Population)
+        % Reference point for hypervolume calculation
+        PopObj = Population.objs;
+        RefPoint = r*(max(PopObj,[],1)-min(PopObj,[],1))+min(PopObj,[],1);
+        MatingPool = TournamentSelection(2,Global.N,-CalHV(Population.objs,RefPoint,Global.N,nSample));
+        Offspring  = GA(Population(MatingPool));    
+        Population = EnvironmentalSelection([Population,Offspring],Global.N,RefPoint,nSample);
+    end
+end

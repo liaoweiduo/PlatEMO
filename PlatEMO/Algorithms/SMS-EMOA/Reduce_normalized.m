58cbe1c5d382b9dccde3e4eef265f763eb78ff24
liaoweiduo <11849249@mail.sustech.edu.cn>
2020-02-27 16:56:51 +0800

    update

---
+++ PlatEMO/Algorithms/SMS-EMOA/Reduce_normalized.m
@@ -0,0 +1,46 @@
+function [Population,FrontNo] = Reduce_normalized(Population,FrontNo,r)
+% Delete one solution from the population
+% normalized before operation
+
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2018-2019 BIMK Group. You are free to use the PlatEMO for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
+% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
+% for evolutionary multi-objective optimization [educational forum], IEEE
+% Computational Intelligence Magazine, 2017, 12(4): 73-87".
+%--------------------------------------------------------------------------
+
+    %% normalization
+    MaxValues = max(Population.objs);
+    MinValues = min(Population.objs);
+    totalPopObj = (Population.objs - MinValues) ./ (MaxValues - MinValues);
+
+    %% Identify the solutions in the last front
+    FrontNo   = UpdateFront(totalPopObj,FrontNo);
+    LastFront = find(FrontNo==max(FrontNo));
+    PopObj    = totalPopObj(LastFront,:);
+    [N,M]     = size(PopObj);
+    
+    %% Calculate the contribution of hypervolume of each solution
+    deltaS = inf(1,N);
+    ref = r*(max(PopObj,[],1)-min(PopObj,[],1))+min(PopObj,[],1);
+%     if M == 2
+%         [~,rank] = sortrows(PopObj);
+%         deltaS(rank(1)) = (PopObj(rank(2),1)-PopObj(rank(1),1)) * (ref(2)             -PopObj(rank(1),2));
+%         deltaS(rank(N)) = (ref(1)           -PopObj(rank(N),1)) * (PopObj(rank(N-1),2)-PopObj(rank(N),2));
+%         for i = 2 : N-1
+%             deltaS(rank(i)) = (PopObj(rank(i+1),1)-PopObj(rank(i),1)).*(PopObj(rank(i-1),2)-PopObj(rank(i),2));
+%         end
+    if N > 1
+        deltaS = CalHVC(PopObj,ref,N);
+    end
+    
+    %% Delete the worst solution from the last front
+    [~,worst] = min(deltaS);
+    FrontNo   = UpdateFront(Population.objs,FrontNo,LastFront(worst));
+    Population(LastFront(worst)) = [];
+    
+%     %% de-normalization
+%     Population = PopObj .* (MaxValues - MinValues) + MinValues;
+end
---
+++ PlatEMO/master/getData_normalization.m
@@ -0,0 +1,56 @@
+clear;
+evaluation = 40000;
+
+problems = {
+    @DTLZ1, @DTLZ2, @DTLZ3, @DTLZ4,...
+    @WFG1, @WFG2, @WFG3, @WFG4, @WFG5, @WFG6, @WFG7, @WFG8, @WFG9,...
+    @MinusDTLZ1, @MinusDTLZ2, @MinusDTLZ3, @MinusDTLZ4,...
+    @MinusWFG1, @MinusWFG2, @MinusWFG3, @MinusWFG4, @MinusWFG5,...
+    @MinusWFG6, @MinusWFG7, @MinusWFG8, @MinusWFG9,...
+    };
+
+M = [3,5,8,10];  
+
+parameters = {};
+for m = M
+    if m == 3 
+        algorithms = {@SMSEMOA1_normalized,@SMSEMOA13_12_normalized,@SMSEMOA2_normalized,@SMSEMOA5_normalized};
+    elseif m == 5
+        algorithms = {@SMSEMOA1_normalized,@SMSEMOA5_4_normalized,  @SMSEMOA2_normalized,@SMSEMOA5_normalized};
+    elseif m == 8
+        algorithms = {@SMSEMOA1_normalized,@SMSEMOA3_2_normalized,  @SMSEMOA5_normalized,@SMSEMOA10_normalized};
+    else 
+        algorithms = {@SMSEMOA1_normalized,@SMSEMOA2_normalized,    @SMSEMOA5_normalized,@SMSEMOA10_normalized};
+    end
+    for algorithm = algorithms
+        for problem = problems
+            for run = 1:5
+                parameters{size(parameters,2)+1} = {m,problem,algorithm,run};
+            end
+        end
+    end
+end
+
+total = size(parameters,2);
+parfor i = 1:total
+    m = parameters{i}{1};
+    problem = parameters{i}{2}{1};
+    algorithm = parameters{i}{3}{1};
+    run = parameters{i}{4};
+    
+    if m == 3
+        N = 91;
+    elseif m == 5
+        N = 70;
+    elseif m == 8
+        N = 36;
+    elseif m == 10
+        N = 30;
+    end
+    
+    fprintf('start %d of %d...\n', i, total);
+    main('-algorithm', {algorithm},'-problem', {problem},... 
+    '-N', N, '-M', m, '-evaluation', evaluation,...
+    '-save', 100, '-run', run);
+    fprintf('finish %d of %d...\n', i, total);
+end
